428321cbbc234f4d5be554909dbc2be02cdee04f
devashishsoni98 <devashishsoni926@gmail.com>
2023-09-09 14:36:12 +0530

    ux improvement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { BiSolidRightArrowAlt } from "react-icons/bi";
 import { RiSunLine, RiMoonClearLine } from "react-icons/ri";
+import { ImCross } from "react-icons/im";
 import "./darkTheme.css";
 
 function App() {
@@ -11,8 +12,9 @@ function App() {
   const apiKey = process.env.REACT_APP_WEATHER_API;
   const [inputCity, setInputCity] = useState("");
   const [data, setData] = useState({});
+  const [key, setKey] = useState(0); 
 
-  const getWetherDetails = (cityName) => {
+  const getWeatherDetails = (cityName) => {
     if (!cityName) return;
     const apiURL =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -22,8 +24,9 @@ function App() {
     axios
       .get(apiURL)
       .then((res) => {
-        // console.log("response", res.data);
         setData(res.data);
+        setInputCity("");
+        setKey((prevKey) => prevKey + 1);
       })
       .catch((err) => {
         console.log("err", err);
@@ -35,16 +38,21 @@ function App() {
   };
 
   const handleSearch = () => {
-    getWetherDetails(inputCity);
+    getWeatherDetails(inputCity);
+  };
+
+  const handleRemoveResult = () => {
+    setData({});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleSearch();
   };
 
   const toggleTheme = () => {
     setIsDarkTheme((prevTheme) => !prevTheme);
   };
-
-  // useEffect(()=>{
-  //   getWetherDetails("delhi")
-  // }, [])
 
   return (
     <div className={`col-md-12 divv ${isDarkTheme ? "dark-theme" : ""}`}>
@@ -52,11 +60,20 @@ function App() {
         <button className="btn button-s" type="button" onClick={toggleTheme}>
           {isDarkTheme ? <RiMoonClearLine /> : <RiSunLine />}
         </button>
-        <h1 className="heading">Check Weather Now!</h1>
+        <h1 data-aos="zoom-in" data-aos-duration="600" className="heading">
+          Check Weather Now!
+        </h1>
 
-        <div className="logo">DS</div>
+        <div data-aos="zoom-in" data-aos-duration="600" className="logo">
+          DS
+        </div>
 
-        <div className="d-grid gap-3 col-4 mt-4">
+        <form
+          data-aos="zoom-in"
+          data-aos-duration="600"
+          className="d-grid gap-3 col-4 mt-4"
+          onSubmit={handleSubmit}
+        >
           <input
             type="text"
             className="form-control"
@@ -64,21 +81,31 @@ function App() {
             value={inputCity}
             onChange={handleChangeInput}
           />
-          <button className="btn button" type="button" onClick={handleSearch}>
+          <button className="btn button" type="submit">
             Search
           </button>
-        </div>
+        </form>
         {Object.keys(data).length > 0 && (
-          <div className="col-md-12 text-center mt-5">
-            <div className=" wetherResultBox">
+          <div
+            className="col-md-12 text-center mt-5"
+            data-aos="flip-left"
+            data-aos-duration="1000"
+            key={key} 
+          >
+            <div className="wetherResultBox">
               <img
                 className="weathorIcon"
                 src="https://cdn.pixabay.com/photo/2017/12/06/05/19/sun-3000986_1280.png"
               />
+              <button className="btn btn-remove" onClick={handleRemoveResult}>
+                <ImCross />
+              </button>
 
-              <h5 className="weathorCity">{data?.name}</h5>
+              <h5 className="weathorCity" data-aos="flip-left">
+                {data?.name}
+              </h5>
               <h6 className="weathorTemp">
-                Temprature <BiSolidRightArrowAlt className="icon" />{" "}
+                Temperature <BiSolidRightArrowAlt className="icon" />{" "}
                 {(data?.main?.temp - 273.15).toFixed(1)}°C
               </h6>
               <h6 className="weathorTemp">
